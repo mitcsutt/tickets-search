779e7ef3020a81f41dd7dbd4bd4e895d29405b95
Mitchell Sutton <mitchellsutton2@gmail.com>
2021-05-17 16:45:22 +1000

    Added the simplest version of the console

--- TicketsSearch/Program.cs
+++ TicketsSearch/Program.cs
@@ -6,7 +6,30 @@ namespace TicketsSearch
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Search for the values");
+
+            var open = true;
+            while (open)
+            {
+                var command = Console.ReadLine();
+                var commandSplit = command.Split(" ");
+                if(commandSplit.Length > 1)
+                {
+                    switch (commandSplit[0])
+                    {
+                        case "search":
+                            Console.WriteLine("Correct value");
+                            break;
+                        default:
+                            Console.WriteLine("Invalid command line argument");
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input");
+                }
+            }
         }
     }
 }
